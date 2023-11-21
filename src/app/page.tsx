34dc3ab072983920322c55c0ca9f2a62765b5492
Umid <umid-berdiev82@mail.ru>
2023-11-21 14:33:30 +0500

    sidebar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-import Image from "next/image";
+"use client";
+
 import { Card } from "flowbite-react";
 
 export default function Home() {
   return (
-    <Card href="#" className="max-w-sm">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far,
-        in reverse chronological order.
-      </p>
-    </Card>
+    <div className="grid grid-cols-2 gap-6">
+      {Array(10)
+        .fill(0)
+        .map((i) => (
+          <Card href="#" className="max-w-sm" key={i}>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+          </Card>
+        ))}
+    </div>
   );
 }
